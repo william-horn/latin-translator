0fd1d8290ae1a8ff2ad98bac9d08ce41c0eade08
William Horn <williamjosephhorn@gmail.com>
2025-09-26 13:34:34 -0400

    added word ending class

--- src/classes/Translator.java
+++ src/classes/Translator.java
@@ -15,18 +15,35 @@ public class Translator {
 	final private static Translator translator = new Translator();
 
 	// Built during constructor
+	final public HashMap<String, WordEnding> wordEndings = new HashMap<>();
+
 	private Translator() {
+		wordEndings.put("orum", new NounEnding());
+	}
+
+	private WordEnding getWordEnding(String token) {
+		for (Entry<String, WordEnding> entry : this.wordEndings.entrySet()) {
+			String entryEnding = entry.getKey();
+			String tokenEnding = token.substring(token.length() - entryEnding.length());
+
+			if (tokenEnding.equals(entryEnding)) {
+				return entry.getValue();
+			}
+		}
+
+		return null;
 	}
 
 	public String toEnglish(String latinString) {
 		String[] tokens = latinString.split("\\s+");
+		StringBuilder translated = new StringBuilder();
 
 		for (int i = 0; i < tokens.length; i++) {
 			String token = tokens[i];
-			Declension.decline(token);
+			WordEnding wordEnding = this.getWordEnding(token);
 		}
 
-		return "";
+		return translated.toString();
 	}
 
 	public static Translator getInstance() {
---
+++ src/classes/VerbEnding.java
@@ -0,0 +1,5 @@
+package classes;
+
+public class VerbEnding extends WordEnding {
+
+}
